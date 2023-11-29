4ff62c7033f7332ad98524ef2ac8e5ef68143356
alyhenr <alyssonhra99@gmail.com>
2023-11-29 19:00:49 -0300

    fix(db-migration): description and price for products

--- src/app.js
+++ src/app.js
@@ -14,11 +14,6 @@ app
     .use(cors())
     .use(express.json())
     .get("/health", (_, res) => { res.send("Server is up!") })
-    .post("/health", (req, res) => {
-        console.log(req.files);
-        if (req.body) res.send(req.body);
-        else res.send({});
-    })
     .use("/clients", clientsRouter)
     .use("/products", productsRouter)
     .use("/categories", categoriesRouter)
