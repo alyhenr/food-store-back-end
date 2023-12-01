56a7bdbab19c8b737ce605e56e62d7c46e75d062
alyhenr <alyssonhra99@gmail.com>
2023-11-30 23:37:43 -0300

    logging interal server errors

--- src/middlewares/errors-handler.js
+++ src/middlewares/errors-handler.js
@@ -4,6 +4,7 @@ export default (err, _req, res, _next) => {
     if (err?.status) {
         res.status(err.status).send(err.message);
     } else {
+        console.log(err);
         res.status(httpStatus.INTERNAL_SERVER_ERROR).send(
             "Please try again in a few seconds."
         );
